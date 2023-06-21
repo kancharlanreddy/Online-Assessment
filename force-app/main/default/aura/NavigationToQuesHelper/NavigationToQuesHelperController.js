70107edf2a7a0ab90960f029ce051108d8cfebd3
Narendar <kancharlanreddy@gmail.com>
2023-06-21 10:38:04 +0530

    1 Week Changes

--- force-app/main/default/aura/NavigationToQuesHelper/NavigationToQuesHelperController.js
+++ force-app/main/default/aura/NavigationToQuesHelper/NavigationToQuesHelperController.js
@@ -3,9 +3,13 @@
         var myPageRef = component.get("v.pageReference");
          var candId = myPageRef.state.c__candId;
         var assessId=myPageRef.state.c__assessId;
-         console.log('Candidate Id : ',candId);
+        var assessName=myPageRef.state.c__assessName;
+        var candName = myPageRef.state.c__candName;
+        // console.log('Candidate Id : ',candId);
          component.set("v.candId", candId);
-        console.log('Assessment Id : ',assessId);
+         component.set("v.candName", candName);
+      //  console.log('Assessment Id : ',assessId);
         component.set("v.assessId", assessId);
+        component.set("v.assessName", assessName);
     }
 })
